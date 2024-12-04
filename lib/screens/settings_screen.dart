1e3759b796721a93b6b5ca8ef7ed73d24ce37af8
m1kjeck <58119601+m1kjeck@users.noreply.github.com>
2024-12-04 14:45:19 +0200

    Add change password logic to settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -11,10 +11,152 @@ class SettingsScreen extends ConsumerStatefulWidget {
   const SettingsScreen({super.key});
 
   @override
-  ConsumerState<SettingsScreen> createState() => _ProfileScreenState();
+  ConsumerState<SettingsScreen> createState() => _SettingsScreenState();
 }
 
-class _ProfileScreenState extends ConsumerState<SettingsScreen> {
+class _SettingsScreenState extends ConsumerState<SettingsScreen> {
+
+  Future<void> _reauthenticateUser() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final credential = EmailAuthProvider.credential(
+        email: user.email!,
+        password: await _showReauthenticateDialog(),
+      );
+      await user.reauthenticateWithCredential(credential);
+    }
+  }
+
+  Future<void> _changePassword() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null &&
+        user.providerData.any((p) => p.providerId == 'password')) {
+      try {
+        await _reauthenticateUser();
+        String newPassword = await _showChangePasswordDialog();
+        if (newPassword.isNotEmpty) {
+          if (newPassword.length >= 8) {
+            await user.updatePassword(newPassword);
+
+            Navigator.of(context).pop();
+            ScaffoldMessenger.of(context).clearSnackBars();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Password changed successfully')),
+            );
+          } else {
+            Navigator.of(context).pop();
+            ScaffoldMessenger.of(context).clearSnackBars();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Password must be at least 8 characters long')),
+            );
+          }
+        }
+      } on FirebaseAuthException catch (e) {
+        Navigator.of(context).pop();
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.message ?? 'An error occurred')),
+        );
+      }
+    } else {
+      Navigator.of(context).pop();
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('Cannot change password for Google account')),
+      );
+    }
+  }
+
+  Future<String> _showChangePasswordDialog() async {
+    String newPassword = '';
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Change Password'),
+          content: TextField(
+            onChanged: (value) {
+              newPassword = value;
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter new password',
+              hintStyle: Theme.of(context)
+                  .textTheme
+                  .labelMedium!
+                  .copyWith(fontSize: 20),
+              border: const OutlineInputBorder(),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: ourPinkColor),
+              ),
+            ),
+            obscureText: true,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Change'),
+            ),
+          ],
+        );
+      },
+    );
+    return newPassword.trim();
+  }
+
+  Future<String> _showReauthenticateDialog() async {
+    String password = '';
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Reauthenticate'),
+          content: TextField(
+            onChanged: (value) {
+              password = value;
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter your current password',
+              hintStyle: Theme.of(context)
+                  .textTheme
+                  .labelMedium!
+                  .copyWith(fontSize: 20),
+              border: const OutlineInputBorder(),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: ourPinkColor),
+              ),
+            ),
+            obscureText: true,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Reauthenticate'),
+            ),
+          ],
+        );
+      },
+    );
+    return password.trim();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -22,6 +164,26 @@ class _ProfileScreenState extends ConsumerState<SettingsScreen> {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      await _changePassword();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      alignment: Alignment.centerLeft,
+                      backgroundColor: ourDarkColor,
+                    ),
+                    child: Text(
+                      'Change Password',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                )
+              ],
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
